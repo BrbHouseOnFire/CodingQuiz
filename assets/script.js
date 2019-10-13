8c9f0fb9446ca3a64ad8173bc9fe10cd1d71f5ca
Brb House on Fire <bobbywoj@sbcglobal.net>
2019-10-13 13:31:47 -0500

    quiz updated up until HS page

--- assets/script.js
+++ assets/script.js
@@ -33,6 +33,8 @@ var isCorrect = false;
 var userSelection = -1;
 var currentQuestion = 0;
 var timeScore = -1;
+var scoreChart = [];
+var nameInput = "";
 
 // init
 renderStartPage();
@@ -42,7 +44,7 @@ renderStartPage();
 
 function td() {
     // For debugging/testing purposes
-    alert("TOUCHDOWN! Take a break");
+    alert("TOUCHDOWN! -takeabreak-");
 }
 
 function runQuiz() {
@@ -65,6 +67,16 @@ function runQuiz() {
 
 function runTimer() {
     var timerInterval = setInterval(function() {
+        if(timeLeft === "HS") {
+            // special case for entering HighScore page during quiz.
+            // end interval
+            clearInterval(timerInterval);
+            // reset timer
+            timeLeft = 0;
+            $("#timerID").text(`Time Left: ${timeLeft}`);
+            // return without triggering endQuiz page.
+            return;
+        }
         timeLeft--;
         console.log("time:" + timeLeft);
         $("#timerID").text(`Time Left: ${timeLeft}`);
@@ -86,10 +98,14 @@ function endQuiz() {
 
 
 function nextQuestion(currentQuestion) {
-    answered = false;
-    isCorrect = false;
+    // Set TIMEOUT to display if right or wrong. *************************************
+    
+    // clear page and move to next question.
     clearPage();
     renderQuestion(currentQuestion);
+    // reset values
+    answered = false;
+    isCorrect = false;
 }
 
 function renderQuestion(index) {
@@ -101,6 +117,7 @@ function renderQuestion(index) {
     newQuestion.text(questionText);
     bodyContainer.append(newQuestion);
     renderChoices(index);
+    
 }
 
 function renderChoices(index) {
@@ -115,15 +132,18 @@ function createAButton(questionNumber, choicesIndex) {
     // returns a customized button based on the given question
     var buttonText = questions[questionNumber].choices[choicesIndex];
     var correctQ = false;
-    // double == instead of triple for different typecasting.
+    // double == instead of triple to account for typecasting.
     if (questions[questionNumber].answer == choicesIndex) {
         correctQ = true;
     }
-    return newButton = `<button class="quizButton qbutton" selection="${choicesIndex}" id="${questionNumber},${choicesIndex}" isCorrect="${correctQ}">${buttonText}</button>`;
+    // return customized buttons
+    return newButton = `<button class="quizButton qbutton button--quiz" selection="${choicesIndex}" id="${questionNumber},${choicesIndex}" isCorrect="${correctQ}">${buttonText}</button>`;
 }
 
 function renderStartPage() {
+    // create a title
     bodyContainer.append(`<div id="bodyTextID" class="bodytext">Nobody passes this quiz. Nobody!</div>`);
+    // create a fancy button
     bodyContainer.append(`<div class = "buttoncontainer flex jcsa" id = "buttonContainer"></div>`);
     bodyContainer.append(`<button class="startButton button--start"
     id="sButton"><span>S</span><span>t</span><span>a</span><span>r</span><span>t</span>
@@ -131,12 +151,60 @@ function renderStartPage() {
 }
 
 function renderScoreSubmissionPage() {
-    console.log("score: " + score);
-    console.log("timescore: " + timeScore);
-    console.log("timeleft: " + timeLeft);
+    // double check to avoid negative scoring.
+    if (timeScore <= 0) {
+        timeScore = 0;
+    }
+    // update timer to avoid negative on-screen.
     $("#timerID").text(`Time Left: ${timeScore}`);
+    // update score to include remaining time
+    score = score + timeScore;
+    // display header
+    bodyContainer.append(`<div id="bodyTextID" class="bodytext">Quiz Over!</div>`);
+    // display user's score
+    bodyContainer.append(`<div id="bodyTextID" class="bodytext">Your score: ${score}</div>`);
+    // create a new form
+    var newForm = $("<form>");
+    newForm.attr("id", "newFormID");
+    // create a div inside the form to house the user submission
+    var newDiv = $("<div>");
+    newDiv.addClass("submission");
+    newDiv.attr("id", "submissionID");
+    // add a label and input to the submission form
+    newForm.append(`<label for="name">Enter your name: </label>`);
+    newForm.append(`<input type="text" name="nameIn" id="nameInput">`);
+    // append the form to the page
+    bodyContainer.append(newForm);
+    // append a fancy button
+    bodyContainer.append(`<div class = "buttoncontainer flex jcsa" id = "buttonContainer"></div>`);
+    bodyContainer.append(`<button class="saveButton button--start"id="saveButton"><span>S</span><span>a</span><span>v</span><span>e</span></button>`)
+}
 
-    
+function renderHighSchorePage() {
+    clearPage();
+    // display title
+    bodyContainer.append(`<div id="bodyTextID" class="bodytext">High Scores!</div>`);
+
+    // display high scores
+    // get local storage
+    var string = localStorage.getItem("highScores");
+    var object = JSON.parse(string);
+    // check if empty.
+    if (string === null) {
+        // if so, display: "no high scores yet"
+    }
+    else {
+        // else, display local storage results.
+    }
+
+    // display retry button
+    // display clear high scores button
+    bodyContainer.append(`<div class = "buttoncontainer flex jcsa" id ="buttonContainer"></div>`);
+    var buttonContainer = $("#buttonContainer");
+    buttonContainer.append(`<button class="retryButton button--start"id="saveButton"><span>R</span><span>e</span><span>t</span><span>r</span><span>y</span><span>?</span></button>`)
+    buttonContainer.append(`<button class="clearButton button--start"id="saveButton"><span>C</span><span>l</span><span>e</span><span>a</span><span>r</span></button>`)
+
+
 }
 
 function clearPage() {
@@ -147,12 +215,15 @@ function clearPage() {
 // -------------------------------------------------------------------
 // event functions
 // click HS
-        // find score object in local storage
-        // parse object for screen
-        // display parsed object in html
+$("#highScoreButton").on("click", function() {
+    // goto High Score Page
+    timeLeft = "HS";
+    renderHighSchorePage();
+});
 
 // click start quiz
 $("#sButton").on("click", function() {
+    // prevent accidental clickage.
     event.stopPropagation();
     event.stopImmediatePropagation();
     // clear out the start page
@@ -162,6 +233,7 @@ $("#sButton").on("click", function() {
 });
 
 $("#bodyContainer").on("click", ".quizButton", function() {
+    // prevent accidental clickage.
     event.stopPropagation();
     event.stopImmediatePropagation();
     // mark that they've chosen an answer
@@ -175,7 +247,13 @@ $("#bodyContainer").on("click", ".quizButton", function() {
     }
     else {
         isCorrect = false;
-        timeLeft -= 15;
+        if (timeLeft <= -15) {
+            // Do nothing
+            // Prevent multi-button presses
+        }
+        else {
+            timeLeft -= 15;
+        }
     }
     currentQuestion++;
     if (currentQuestion < questions.length) {
@@ -188,8 +266,43 @@ $("#bodyContainer").on("click", ".quizButton", function() {
 });
 
 
+$("#bodyContainer").on("click", "#saveButton", function() {
+    nameInput = $("#nameInput").val();
+    if (nameInput === "") {
+        alert("A name is required for submission.");
+    }
+    else {
+        // store name and score to local storage.
+        // get and parse local storage
+        var string = localStorage.getItem("highScores");
+        var object = JSON.parse(string);
+        // check if empty.
+        if (string === null) {
+            // if so, create first entry
+            scoreChart[0] = {
+                name: nameInput,
+                score: score
+            }
+            localStorage.setItem("highScores", JSON.stringify(scoreChart));
+        }
+        else {
+            // else, push onto end of the array.
+            scoreChart = object;
+            scoreChart.push({
+                name: nameInput,
+                score: score
+                }
+            );
+            localStorage.setItem("highScores", JSON.stringify(scoreChart));
+        }
+        // Goto High Score Page
+        renderHighSchorePage();
+    }
+});
 
-// enter initials and submit score, go to HS
+
+
+
 
 
 // clear HS
